79edfab925d7803f9b4d535902c937377dd424c7
Rieyani <gunangeh7@gmail.com>
2023-09-29 23:42:19 +0900

    perbaiki rule

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
+import 'config/build_material_color.dart';
+import 'resources/app_resources.dart';
+import 'resources/app_text_theme.dart';
 
 void main() {
   runApp(
@@ -10,6 +12,10 @@ void main() {
       title: "Application",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
+      theme: ThemeData(
+        textTheme: AppTextTheme.blackMountainView,
+        primarySwatch: buildMaterialColor(AppColors.primary),
+      ),
     ),
   );
 }
