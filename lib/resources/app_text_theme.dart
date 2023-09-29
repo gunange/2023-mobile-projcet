fcc82166846804c65dd09ba355756b83efacaa77
Rieyani <gunangeh7@gmail.com>
2023-09-29 23:07:37 +0900

    ganti text theme

--- lib/resources/app_text_theme.dart
+++ lib/resources/app_text_theme.dart
@@ -7,123 +7,82 @@ import 'app_text_style.dart';
 
 class AppTextTheme {
   static const TextTheme blackCupertino = TextTheme(
-    overline: TextStyle(
-        fontFamily: '.SF UI Text',
-        color: AppColors.black,
-        decoration: TextDecoration.none),
-    caption: TextStyle(
-        fontFamily: '.SF UI Text',
-        color: Colors.black87,
-        decoration: TextDecoration.none),
-    button: TextStyle(
-        fontFamily: '.SF UI Text',
-        color: AppColors.black,
-        decoration: TextDecoration.none),
-    bodyText1: TextStyle(
-        fontFamily: '.SF UI Text',
-        color: AppColors.black,
-        decoration: TextDecoration.none),
-    bodyText2: TextStyle(
-        fontFamily: '.SF UI Text',
-        color: AppColors.black,
-        decoration: TextDecoration.none),
-    headline1: TextStyle(
-        fontFamily: '.SF UI Text',
-        color: AppColors.black,
-        decoration: TextDecoration.none),
-    headline2: TextStyle(
-        fontFamily: '.SF UI Text',
-        color: AppColors.black,
-        decoration: TextDecoration.none),
-    headline3: TextStyle(
-        fontFamily: '.SF UI Text',
-        color: AppColors.black,
-        decoration: TextDecoration.none),
-    headline4: TextStyle(
-        fontFamily: '.SF UI Text',
-        color: AppColors.black,
-        decoration: TextDecoration.none),
-    headline5: TextStyle(
-        fontFamily: '.SF UI Text',
-        color: AppColors.black,
-        decoration: TextDecoration.none),
-    headline6: TextStyle(
-        fontFamily: '.SF UI Text',
-        color: AppColors.black,
-        decoration: TextDecoration.none),
-    subtitle1: TextStyle(
-        fontFamily: '.SF UI Text',
-        color: AppColors.black,
-        decoration: TextDecoration.none),
-    subtitle2: TextStyle(
-        fontFamily: '.SF UI Text',
-        color: AppColors.black,
-        decoration: TextDecoration.none),
+    overline:
+        TextStyle(color: AppColors.black, decoration: TextDecoration.none),
+    caption: TextStyle(color: Colors.black87, decoration: TextDecoration.none),
+    button: TextStyle(color: AppColors.black, decoration: TextDecoration.none),
+    bodyText1:
+        TextStyle(color: AppColors.black, decoration: TextDecoration.none),
+    bodyText2:
+        TextStyle(color: AppColors.black, decoration: TextDecoration.none),
+    headline1:
+        TextStyle(color: AppColors.black, decoration: TextDecoration.none),
+    headline2:
+        TextStyle(color: AppColors.black, decoration: TextDecoration.none),
+    headline3:
+        TextStyle(color: AppColors.black, decoration: TextDecoration.none),
+    headline4:
+        TextStyle(color: AppColors.black, decoration: TextDecoration.none),
+    headline5:
+        TextStyle(color: AppColors.black, decoration: TextDecoration.none),
+    headline6:
+        TextStyle(color: AppColors.black, decoration: TextDecoration.none),
+    subtitle1:
+        TextStyle(color: AppColors.black, decoration: TextDecoration.none),
+    subtitle2:
+        TextStyle(color: AppColors.black, decoration: TextDecoration.none),
   );
 
   static const TextTheme blackMountainView = TextTheme(
     overline: TextStyle(
-        fontFamily: 'Lexend',
         color: AppColors.mainTextColor,
         fontSize: AppTextStyle.fontSizeNormal,
         decoration: TextDecoration.none),
     caption: TextStyle(
-        fontFamily: 'Lexend',
         color: Colors.black87,
         fontSize: AppTextStyle.fontSizeNormal,
         decoration: TextDecoration.none),
     button: TextStyle(
-        fontFamily: 'Lexend',
         color: AppColors.mainTextColor,
         fontSize: AppTextStyle.fontSizeNormal,
         decoration: TextDecoration.none),
     bodyText1: TextStyle(
-        fontFamily: 'Lexend',
         color: AppColors.mainTextColor,
         fontSize: AppTextStyle.fontSizeNormal,
         decoration: TextDecoration.none),
     bodyText2: TextStyle(
-        fontFamily: 'Lexend',
         color: AppColors.mainTextColor,
         fontSize: AppTextStyle.fontSizeNormal,
         decoration: TextDecoration.none),
     headline1: TextStyle(
-        fontFamily: 'Lexend',
         color: AppColors.mainTextColor,
         fontSize: AppTextStyle.fontSizeNormal,
         decoration: TextDecoration.none),
     headline2: TextStyle(
-        fontFamily: 'Lexend',
         color: AppColors.mainTextColor,
         fontSize: AppTextStyle.fontSizeNormal,
         decoration: TextDecoration.none),
     headline3: TextStyle(
-        fontFamily: 'Lexend',
         color: AppColors.mainTextColor,
         fontSize: AppTextStyle.fontSizeNormal,
         decoration: TextDecoration.none),
     headline4: TextStyle(
-        fontFamily: 'Lexend',
         color: AppColors.mainTextColor,
         fontSize: AppTextStyle.fontSizeNormal,
         decoration: TextDecoration.none),
     headline5: TextStyle(
-        fontFamily: 'Lexend',
         color: AppColors.mainTextColor,
         fontSize: AppTextStyle.fontSizeNormal,
         decoration: TextDecoration.none),
     headline6: TextStyle(
-        fontFamily: 'Lexend',
         color: AppColors.mainTextColor,
         fontSize: AppTextStyle.fontSizeNormal,
         decoration: TextDecoration.none),
     subtitle1: TextStyle(
-        fontFamily: 'Lexend',
         color: AppColors.mainTextColor,
         fontSize: AppTextStyle.fontSizeNormal,
         decoration: TextDecoration.none),
     subtitle2: TextStyle(
-        fontFamily: 'Lexend',
         color: AppColors.mainTextColor,
         fontSize: AppTextStyle.fontSizeNormal,
         decoration: TextDecoration.none),
